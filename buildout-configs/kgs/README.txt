4e76aa2b5d838c660dc56c2f8d9f12a263b274c5
Olimpiu Rob <olimpiu.rob@gmail.com>
2013-06-21 16:03:58 +0200

    Refs #14827 EEA; added initial implementation of buildout

---
+++ buildout-configs/kgs/README.txt
@@ -0,0 +1,2 @@
+TODO: kgs description
+
---
+++ install.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+DISTRIBUTE=`grep "distribute\s*\=\s*" versions.cfg | sed 's/ *$//g' | sed 's/=$//g' | sed 's/\s*=\s*/==/g'`
+ZCBUILDOUT=`grep "zc\.buildout\s*=\s*" versions.cfg | sed 's/\s*=\s*/==/g'`
+
+if [ -z "$DISTRIBUTE" ]; then
+  DISTRIBUTE="distribute"
+fi
+
+if [ -z "$ZCBUILDOUT" ]; then
+  ZCBUILDOUT="zc.buildout"
+fi
+
+if [ -s "bin/activate" ]; then
+  echo "Updating distribute: ./bin/easy_install" $SETUPTOOLS
+  ./bin/easy_install $DISTRIBUTE
+
+  echo "Updating zc.buildout: ./bin/easy_install" $ZCBUILDOUT
+  ./bin/easy_install $ZCBUILDOUT
+
+  echo ""
+  echo "============================================================="
+  echo "Buildout is already installed."
+  echo "Please remove bin/activate if you want to re-run this script."
+  echo "============================================================="
+  echo ""
+
+  exit 0
+fi
+
+echo "Installing virtualenv"
+wget --no-check-certificate "https://raw.github.com/pypa/virtualenv/master/virtualenv.py" -O "/tmp/virtualenv.py"
+
+echo "Running: python2.6 /tmp/virtualenv.py --clear ."
+python2.6 "/tmp/virtualenv.py" --clear .
+rm /tmp/virtualenv.py*
+
+#echo "Updating distribute: ./bin/easy_install" $SETUPTOOLS
+#./bin/easy_install $DISTRIBUTE
+
+echo "Installing zc.buildout: $ ./bin/easy_install" $ZCBUILDOUT
+./bin/easy_install $ZCBUILDOUT
+
+echo "Disabling the SSL CERTIFICATION for git"
+git config --global http.sslVerify false
+
+echo ""
+echo "==========================================================="
+echo "All set. Now you can run ./bin/buildout or ./bin/develop rb"
+echo "==========================================================="
+echo ""
